3f295603a12d9e9f4ea13adba90b7dac24774cef
TheBluemist1404 <hoang.ct1lhp@gmail.com>
2024-12-13 16:26:12 +0700

    update variable in css, header/footer

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return(
         <div className="footer">
-            <div className="right">
+            <div className="left">
                 <img src="./src/assets/bg-logo.svg" alt="" />
                 <div>
                     <h1 className="team">DevTeam 1</h1>
@@ -12,7 +12,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="left">
+            <div className="right">
                 <h1>Contact Us:</h1>
                 <div style={{height: "20px"}}></div>
                 <p>cat@devteam1.com</p>
