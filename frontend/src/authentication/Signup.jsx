ba57b6febb5fb7a9bcea5e327b91a1b826ac2288
Bui Duc Tuong <tuongbuiduc1906@gmail.com>
2024-12-15 15:23:56 +0700

    Update Signup.jsx

---
+++ frontend/src/authentication/Signup.jsx
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="register.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body background="image 9.png">  
+  <form action="action_page.php" method="post">
+    <div class="taskbar">
+        <nav class="link"> 
+          <a class="logo"><img src="CAT-removebg-preview.png" alt="logo"></a>
+          <a>About</a>
+          <a>Forum</a>
+          <a>LiveCode</a>
+          <a>Contact</a>
+        </nav>
+    </div>
+  </form>
+  <form action="action_page.php" method="post">
+    <div class="container">
+      <div class="signup">Join The CATs</div>
+      <div class="name">
+        <div class="wave-group">
+          <input required="" type="text" class="input">
+          <span class="bar"></span>
+          <label class="label">
+            <span class="label-char" style="--index: 0">N</span>
+            <span class="label-char" style="--index: 1">a</span>
+            <span class="label-char" style="--index: 2">m</span>
+            <span class="label-char" style="--index: 3">e</span>
+          </label>
+        </div>
+        <a class="user"><i class="fa-solid fa-user" style="font-size: 25px; color: rgb(255, 255, 255);"></i></a>
+      </div>
+      <div class="email">
+        <div class="wave-group">
+          <input required="" type="text" class="input">
+          <span class="bar"></span>
+          <label class="label">
+            <span class="label-char" style="--index: 0">E</span>
+            <span class="label-char" style="--index: 1">m</span>
+            <span class="label-char" style="--index: 2">a</span>
+            <span class="label-char" style="--index: 3">i</span>
+            <span class="label-char" style="--index: 4">l</span>
+          </label>
+        </div>
+        <a class="e"><i class="fa-regular fa-envelope" style="font-size: 25px; color: rgb(255, 255, 255);"></i></a>
+      </div>
+      <div class="password">
+        <div class="wave-group">
+          <input required="" type="password" class="input">
+          <span class="bar"></span>
+          <label class="label">
+            <span class="label-char" style="--index: 0">P</span>
+            <span class="label-char" style="--index: 1">a</span>
+            <span class="label-char" style="--index: 2">s</span>
+            <span class="label-char" style="--index: 3">w</span>
+            <span class="label-char" style="--index: 4">o</span>
+            <span class="label-char" style="--index: 5">r</span>
+            <span class="label-char" style="--index: 6">d</span>
+          </label>
+        </div>
+        <a class="lock"><i class="fa-solid fa-lock" style="font-size: 25px; color: rgb(255, 255, 255);"></i></a>
+      </div>
+      </div>
+      <button type="submit">login</button>
+      <div class="reg">Don't have an account?<p> Register</p></div>
+      <a class="fb"><i class="fab fa-facebook" style="font-size: 30px; color: rgb(255, 255, 255);"></i></a>
+      <a class="gg"><i class="fa-brands fa-google" style="font-size: 30px; color: rgb(255, 255, 255);"></i></a>
+    </div>
+  </form>
+    
+</body>
+</html>
