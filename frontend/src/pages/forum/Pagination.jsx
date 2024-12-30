df5a4e270bca23c8d0e6b0a39b9805d70d731f8b
namnguyenhcmut <wsnguyennam123@gmail.com>
2024-12-30 15:06:42 +0700

    Add files via upload

---
+++ frontend/src/pages/forum/Pagination.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Pagination() {
+  return (
+    <div className="pagination">
+      <button className="page-button prev">&laquo; Prev</button>
+      <span className="page-number">1</span>
+      <span className="page-number">2</span>
+      <span className="page-number">3</span>
+      <span className="page-number">4</span>
+      <button className="page-button next">Next &raquo;</button>
+    </div>
+  );
+}
+
+export default Pagination;
