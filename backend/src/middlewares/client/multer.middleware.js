d3c32d4b951351423250fdb6307a39aa82eab8bb
Jikei <khangphamcs@gmail.com>
2024-12-30 21:57:31 +0700

    feat: add upload to cloud config

---
+++ backend/src/middlewares/client/multer.middleware.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    const uniquePrefix = Date.now() + file.originalname;
+    cb(null, `${uniquePrefix}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      req.fileValidationError = "Forbidden extension";
+      cb(null, false);
+    }
+  },
+});
+
+module.exports = upload;
---
+++ backend/src/utils/streamUpload.js
@@ -0,0 +1,27 @@
+const streamifier = require('streamifier');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
+module.exports.streamUpload = buffer => {
+  return new Promise((resolve, reject) => {
+    let stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: 'auto',
+      },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      },
+    );
+
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
