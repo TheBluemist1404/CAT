bb980b9131bdece07c6a1902cbaeec2750bfa76e
namnguyenhcmut <wsnguyennam123@gmail.com>
2024-12-30 15:05:32 +0700

    Delete CAT-main directory

--- CAT-main/backend/src/routes/client/token.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../../controllers/client/token.controller");
-const validation = require("../../middlewares/client/validation.middleware");
-
-router.post('/', validation.validateRefreshToken, controller.getNewToken);
-module.exports = router;
