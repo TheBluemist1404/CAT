df205ef4ee97d48ed9e78450547cdfad80b2010a
Jikei <khangphamcs@gmail.com>
2025-01-03 13:27:33 +0700

    feat: add oauth2 using google

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 require('dotenv').config();
-const cors = require("cors");
+const cors = require('cors');
 
-const database = require("./config/database"); 
+const database = require('./config/database');
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
-const router = require("./routes/client/index.route");
+const router = require('./routes/client/index.route');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +16,20 @@ app.use(cors());
 router(app);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
 
+// const rootURL = 'https://accounts.google.com/o/oauth2/v2/auth';
+// const options = {
+//   redirect_uri: process.env.REDIRECT_URI,
+//   client_id: process.env.CLIENT_ID,
+//   access_type: 'offline',
+//   response_type: 'code',
+//   prompt: 'consent',
+//   scope: [
+//     'https://www.googleapis.com/auth/userinfo.profile',
+//     'https://www.googleapis.com/auth/userinfo.email',
+//   ].join(' '),
+// };
+// const qs = new URLSearchParams(options);
+// console.log(`${rootURL}?${qs.toString()}`);
--- backend/src/routes/client/index.route.js
+++ backend/src/routes/client/index.route.js
@@ -1,4 +1,5 @@
 const authRoutes = require('./auth.route');
+const oauthRoutes = require('./oauth.route')
 const tokenRoutes = require('./token.route');
 const postRoutes = require('./post.route');
 const profileRoutes = require('./profile.route');
@@ -7,6 +8,7 @@ module.exports = app => {
   const version = '/api/v1';
 
   app.use(version + '/auth', authRoutes);
+  app.use(version + '/oauth', oauthRoutes);
   app.use(version + '/token', tokenRoutes);
   app.use(version + '/forum', postRoutes);
   app.use(version + '/profile', profileRoutes);
