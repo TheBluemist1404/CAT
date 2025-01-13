e4c803d4260119e7b9af4463c1be850ab02476db
TheBluemist1404 <hoang.ct1lhp@gmail.com>
2025-01-13 10:29:26 +0700

    feat: design UI for auth/forgot -fix: enhance rendering posts for forum

--- frontend/src/authentication/Forgot.jsx
+++ frontend/src/authentication/Forgot.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
+import './forgot.scss'
 
 function Forgot() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [getOtp, setGetOtp] = useState(false)
   const [otp, setOtp] = useState("");
   const [reset, setReset] = useState(false);
   const [otpToken, setOtpToken] = useState();
@@ -17,6 +19,9 @@ function Forgot() {
     try {
       console.log(email);
       const response = await axios.post('http://localhost:3000/api/v1/auth/forgot', { email: email })
+      if (response.status === 200) {
+        setGetOtp(true);
+      }
       console.log(response.data)
     } catch (error) {
       console.error("fail to send email", error)
@@ -41,7 +46,7 @@ function Forgot() {
     e.preventDefault();
     try {
       console.log(otpToken)
-      const response = await axios.patch('http://localhost:3000/api/v1/auth/reset-password', { password: pass, confirmPassword: confirm}, {headers: {'Authorization': `Bearer ${otpToken}`}})
+      const response = await axios.patch('http://localhost:3000/api/v1/auth/reset-password', { password: pass, confirmPassword: confirm }, { headers: { 'Authorization': `Bearer ${otpToken}` } })
       console.log(response)
       if (response.status === 200) {
         navigate('/auth/login')
@@ -51,36 +56,44 @@ function Forgot() {
     }
   }
 
+  const renderGetOtop = () => (
+    <div>
+      {!getOtp ? (<form className="enter-email" onSubmit={handleMail}>
+        <h1>Forgot password?</h1>
+        <p>To reset your password, please enter the email address associated with your account</p>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" required="true" placeholder="cat@example.com" onChange={(e) => { setEmail(e.target.value) }} />
+        <button type="submit" style={{ width: '100%' }} >Get OTP</button>
+      </form>) : (<form className="verify-otp" onSubmit={handleOTP}>
+        <label htmlFor="otp">Enter your OTP here</label>
+        <input id="otp" type="text" required="true" placeholder="******" onChange={(e) => { setOtp(e.target.value) }} />
+        <button type="submit">Verify</button>
+      </form>)}
+    </div>
+  )
+
   const renderResetPass = () => (
     <div>
-      <form className="confirm-password" onSubmit={handleReset} style={{ marginTop: '20px' }}>
-        <div>
-          <label htmlFor="reset">New pass word</label>
-          <input id="reset" type="password" onChange={(e) => { setPass(e.target.value) }} style={{ marginLeft: '20px' }} />
-        </div>
-        <div>
-          <label htmlFor="confirm">Confirm password</label>
-          <input id="confirm" type="password" onChange={(e) => { setConfirm(e.target.value) }} style={{ marginLeft: '20px' }} />
-        </div>
-        <button type="submit">Reset password</button>
+      <form className="reset-password" onSubmit={handleReset}>
+        <h1>Reset password</h1>
+        <p>Let's get you a new password!</p>
+
+        <label htmlFor="reset">New password</label>
+        <input id="reset" type="password" onChange={(e) => { setPass(e.target.value) }} />
+
+        <label htmlFor="confirm">Confirm new password</label>
+        <input id="confirm" type="password" onChange={(e) => { setConfirm(e.target.value) }} />
+
+        <button type="submit">Reset</button>
       </form>
     </div>
   )
   return (
-    <div className="forget" >
-      <div>
-        <form className="enter-email" onSubmit={handleMail}>
-          <label htmlFor="email">Enter your email to receive varification OTP</label>
-          <input id="email" type="text" onChange={(e) => { setEmail(e.target.value) }} style={{ marginLeft: '20px' }} />
-          <button type="submit" >Get OTP</button>
-        </form>
-        <form className="verify-otp" onSubmit={handleOTP} style={{ marginTop: '20px' }}>
-          <label htmlFor="otp">Enter your OTP here</label>
-          <input id="otp" type="text" onChange={(e) => { setOtp(e.target.value) }} style={{ marginLeft: '20px' }} />
-          <button type="submit">Send</button>
-        </form>
+    <div className="forgot" >
+      <div className="container">
+        {!reset ? (renderGetOtop()) : (
+          renderResetPass())}
       </div>
-      {reset && renderResetPass()}
     </div>
   )
 }
