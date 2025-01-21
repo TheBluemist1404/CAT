e89d62b133215691a012263205d112371e2fcf9a
TheBluemist1404 <hoang.ct1lhp@gmail.com>
2025-01-21 11:37:31 +0700

    feat: verify mail for /auth/forgot

--- backend/src/controllers/client/auth.controller.js
+++ backend/src/controllers/client/auth.controller.js
@@ -7,6 +7,7 @@ const Otp = require('../../models/client/otp.model');
 const { hashingOtp } = require('../../utils/otpHashing');
 const { sendMail } = require('../../utils/sendMail');
 require('dotenv').config();
+const axios = require('axios');
 
 // [POST] /api/v1/auth/signup
 module.exports.signup = async (req, res) => {
@@ -109,6 +110,24 @@ module.exports.logout = async (req, res) => {
 module.exports.forgot = async (req, res) => {
   try {
     const email = req.body.email;
+    const data = {
+      api_key: process.env.EMAIL_VERIFY_API_KEY,
+      email_address: email
+    };
+
+    const mailVerify = await axios.post('https://verify.maileroo.net/check', data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log()
+    if (mailVerify.data && mailVerify.data.error_code !== '') {
+      res.status(404).json({
+        message: 'Invalid email!',
+      });
+      return;
+
+    }
     const user = await User.findOne({
       email: email,
       deleted: false,
