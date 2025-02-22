4c6cc767031f99bb606a863fd22105705d3646a8
Jikei <khangphamcs@gmail.com>
2025-02-22 22:47:08 +0700

    feat: add insert/remove collaborator

--- backend/src/routes/client/project.route.js
+++ backend/src/routes/client/project.route.js
@@ -7,5 +7,8 @@ router.post('/', auth.authenticateToken, projectController.createProject);
 router.get('/:id', auth.authenticateToken, projectController.getProject);
 router.patch('/:id', auth.authenticateToken, projectController.updateProject);
 router.delete('/:id', auth.authenticateToken, projectController.deleteProject);
+router.get('/', auth.authenticateToken, projectController.getProjects);
+router.patch('/:id/collaborators', auth.authenticateToken, projectController.addCollaborator);
+router.patch('/:id/remove-collaborator', auth.authenticateToken, projectController.removeCollaborator);
 
 module.exports = router;
