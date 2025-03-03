fe27388928bb4881b8a6c0ff1e3c7eae495ccb3f
bdtuong <tuongbuiduc1906@gmail.com>
2025-03-03 21:05:28 +0700

    add image file type

--- backend/src/middlewares/client/multer.middleware.js
+++ backend/src/middlewares/client/multer.middleware.js
@@ -12,12 +12,22 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/gif" ||
+      file.mimetype === "image/webp" ||
+      file.mimetype === "image/svg+xml" ||
+      file.mimetype === "image/bmp" ||
+      file.mimetype === "image/tiff"
+    ) {
       cb(null, true);
     } else {
       req.fileValidationError = "Forbidden extension";
       cb(null, false);
     }
+    
   },
 });
 
