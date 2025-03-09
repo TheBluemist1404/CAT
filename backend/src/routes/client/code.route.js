7ba5ad53046b24d0bd3d581542771b2e5e309a63
TheBluemist1404 <hoang.ct1lhp@gmail.com>
2025-03-09 08:12:29 +0700

    feat: convert code sandbox to api call

--- backend/src/routes/client/code.route.js
+++ backend/src/routes/client/code.route.js
@@ -5,5 +5,6 @@ const controller = require('../../controllers/client/code.controller');
 router.post('/execute',  controller.execute)
 // Add this SSE route for log streaming.
 router.get('/logs/:executionId', controller.streamLogs);
+router.post('/push-log', controller.pushLogEndpoint)
 
 module.exports = router
